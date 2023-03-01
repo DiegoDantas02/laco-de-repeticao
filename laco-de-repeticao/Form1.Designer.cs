239df268d9ac3474e7ef6be803b3ae962e4f5fa2
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-03-01 19:25:11 -0300

    Adicionar arquivos de projeto.

---
+++ laco-de-repeticao/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace laco_de_repeticao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listarFor = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listarFor
+            // 
+            this.listarFor.FormattingEnabled = true;
+            this.listarFor.Location = new System.Drawing.Point(48, 36);
+            this.listarFor.Name = "listarFor";
+            this.listarFor.Size = new System.Drawing.Size(157, 303);
+            this.listarFor.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(226, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "clique Aqui";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 383);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listarFor);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listarFor;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ laco-de-repeticao/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laco_de_repeticao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listarFor.Items.Add(1);
+            listarFor.Items.Add(2);
+            listarFor.Items.Add(3);
+            listarFor.Items.Add(4);
+            listarFor.Items.Add(5);
+            listarFor.Items.Add(6);
+            listarFor.Items.Add(7);
+            listarFor.Items.Add(8);
+            listarFor.Items.Add(9);
+            listarFor.Items.Add(10);
+        }
+    }
+}
